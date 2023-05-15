7b0c96f27e01da69209fc2dd1357a7de3fc2fedd
giant-paw <giantpawbusiness@gmail.com>
2023-05-15 16:28:23 +0700

    penambahan class mahasiswa

---
+++ mbrStatikfunc_105/mbrStatikfunc_105.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+    static int nim;
+
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; /*Definisi Function*/ }
+    static int getNim() { return nim; /*Definisi function*/}
+
+    mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
