7c8f3c4fc5b92abb6850c061c7b697cb8a43ecbe
giant-paw <giantpawbusiness@gmail.com>
2023-05-15 13:14:52 +0700

    membuat destructor angka

--- destruktor_105/destruktor_105.cpp
+++ destruktor_105/destruktor_105.cpp
@@ -20,6 +20,13 @@ angka::angka(int i) {				// Constructor
 	isiData();
 }
 
+angka::~angka() {					// Destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
 int main()
 {
 }
